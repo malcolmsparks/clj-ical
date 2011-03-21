09bb4adb8d7f1189540f5642bc5170378528fcea
Malcolm Sparks <malcolm@congreve.com>
2011-03-21 17:27:47 +0000

    Added marginalia.

--- project.clj
+++ project.clj
@@ -19,4 +19,5 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [clj-time "0.1.0-RC1"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [marginalia "0.5.0"]])
